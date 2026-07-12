677614374beeef2a034e5b75fc4a272207bb491f
Sahil Chess <sahilchess09@gmail.com>
2026-07-12 12:45:17 -0400

    Initial commit

---
+++ scripts/hero.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const GRAVITY : int = 1000
+const MAX_VEL : int = 600
+const FLAP_SPEED : int = -500
+const START_POS = Vector2(100,400)
+var flying: bool = false
+var falling: bool = false
+
+
+func _ready():
+	reset()
+	
+func reset():
+	falling = false
+	flying = false
+	position = START_POS
+	set_rotation(0)
+	
+	
